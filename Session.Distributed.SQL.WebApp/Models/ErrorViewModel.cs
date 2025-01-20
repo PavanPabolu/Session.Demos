8e8b25a32c129a566e7d26602888a0ff26b38dd0
PavanPabolu <PavanPabolu.edu@gmail.com>
2025-01-21 01:11:22 +0530

    SQL Server as a distributed session store

---
+++ Session.Distributed.SQL.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Session.Distributed.SQL.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Session.Distributed.SQL.WebApp/Program.cs
@@ -0,0 +1,63 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+
+//c.com/c/678e4058-48d0-8005-88f3-b8aa58d41674
+//p.ai/search/fix-the-error-sql-cache-the-te-wBj6pAK6QgaE8WroTIW4Hw
+
+/*
+//To use SQL Server as a distributed session store
+1.Install Required Package
+Install-package Microsoft.AspNetCore.Session.SqlServer
+
+2.Create Session Table: Run the following command to create the required table
+dotnet sql-cache create "Server=localhost\\SQLEXPRESS;Database=Demo;Trusted_Connection=True;" dbo Session
+
+*/
+
+//3.Configure SQL Server
+builder.Services.AddDistributedSqlServerCache(options =>
+{
+    options.ConnectionString = "Server=localhost\\SQLEXPRESS;Database=Demo;Trusted_Connection=True;";
+    options.SchemaName = "dbo";
+    options.TableName = "Session";
+});
+
+// Configure session to use Redis
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30); // Set session timeout
+    options.Cookie.HttpOnly = true; // Security setting
+    options.Cookie.IsEssential = true; // Essential for GDPR compliance
+});
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+//app.UseAuthorization();
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+
+app.UseSession(); // Enable session middleware
+app.MapDefaultControllerRoute();
+
+
+
+app.Run();
