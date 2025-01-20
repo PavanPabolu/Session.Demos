9e16da87e87f1b1e88aa1101623d732cb18d3a82
PavanPabolu <PavanPabolu.edu@gmail.com>
2025-01-21 00:53:49 +0530

    Distributed-Redis for session

---
+++ Session.Distributed.Redis.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Session.Distributed.Redis.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
