7777bc0f969673c71cf9d409ad8e6b9f53a6a19a
PavanPabolu <PavanPabolu.edu@gmail.com>
2025-02-14 18:48:38 +0530

    upd

--- Session.DistributedCacheStore.SQL.WebApi2/Program.cs
+++ Session.DistributedCacheStore.SQL.WebApi2/Program.cs
@@ -1,8 +1,21 @@
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
+
+//dotnet add package Microsoft.EntityFrameworkCore.SqlServer
+//dotnet add package Microsoft.EntityFrameworkCore.Tools
+//Install-Package Microsoft.EntityFrameworkCore.Tools
+
+//Install-Package Microsoft.Extensions.Caching.SqlServer
+//Install-Package Microsoft.EntityFrameworkCore.SqlServer
+//Install-Package Microsoft.AspNetCore.Identity.EntityFrameworkCore
+
+//dotnet sql-cache create "Data Source=YOUR_SERVER;Initial Catalog=SessionDB;Integrated Security=True;TrustServerCertificate=True" dbo MySessions
+//dotnet sql-cache create "Data Source=localhost\\SQLEXPRESS;Initial Catalog=SessionDB;Integrated Security=True;TrustServerCertificate=True" dbo MySessions
+
+
 
 var app = builder.Build();
 
